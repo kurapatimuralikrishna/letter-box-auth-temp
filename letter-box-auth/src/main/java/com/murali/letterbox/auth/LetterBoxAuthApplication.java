fd0fa637784868986cf66e0f50d259d873494f1f
kurapatimuralikrishna <kuraptimuralikrishna2405@gmail.com>
2022-04-20 17:18:58 +0530

    New main branch.

---
+++ letter-box-auth/src/main/java/com/murali/letterbox/auth/LetterBoxAuthApplication.java
@@ -0,0 +1,13 @@
+package com.murali.letterbox.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterBoxAuthApplication {
+	//Welcome
+	public static void main(String[] args) {
+		SpringApplication.run(LetterBoxAuthApplication.class, args);
+	}
+
+}
